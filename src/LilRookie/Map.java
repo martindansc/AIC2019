a1cc73a9657e1610c5b83e406e7376f06e311903
mariavila <mariava.1213@gmail.com>
2019-07-06 13:05:18 +0200

    Constructor of worker class

---
+++ src/LilRookie/Map.java
@@ -0,0 +1,30 @@
+package LilRookie;
+
+public class Map {
+/*
+A tenir en compte en el mapa:
+- Tipus de terrain
+- Catapulta fire incoming?
+
+Estructures i unitats
+- Static
+    - Base
+        - Health
+    - Recurs
+    - Poble
+        - Lleieltat
+- Dynamic:
+    -> last round seen
+    - Torre
+        - Propietari
+        - Health
+    - Quarter
+        - Propietari
+        - Health
+    - Unitat
+        - Propietari
+        - Tipus
+        - Mes info?
+- Empty
+*/
+}
--- src/LilRookie/Worker.java
+++ src/LilRookie/Worker.java
@@ -1,5 +1,34 @@
 package LilRookie;
+import aic2019.*;
 
 public class Worker {
+    //General direction
+    Direction dirDestination;
+    //Direction used in last round
+    Direction lastDirection;
 
+    //Location of the closest resource
+    Location resource;
+
+    //Generate worker without a fixed destination
+    public Worker(){
+
+        //Get a first random direction
+        /*Generate a random number from 0 to 7, both included*/
+        int randomNumber = (int)(Math.random()*8);
+
+        /*Get corresponding direction*/
+        dirDestination = Direction.values()[randomNumber];
+    }
+
+    //Generate worker with a fixed destination
+    public Worker(Direction destination){
+        dirDestination = destination;
+    }
+
+    //Generate worker with destination to resource
+    public Worker(Location resource){
+
+        //dirDestination = utils.getDirectionFromLocations()
+    }
 }
