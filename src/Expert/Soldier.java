7c3adf5970c862d66a8e95f78440b8770a2d54bd
Erik Martinez <emrbeltane@gmail.com>
2019-07-16 17:16:55 +0200

    Soldier improved micro

--- src/Expert/Soldier.java
+++ src/Expert/Soldier.java
@@ -54,22 +54,25 @@ public class Soldier {
         MicroInfo[] microInfo = new MicroInfo[9];
         for (int i = 0; i < 9; i++) {
             microInfo[i] = new MicroInfo(in.staticVariables.myLocation.add(in.staticVariables.dirs[i]));
+            microInfo[i].updateArea();
         }
 
         boolean enemies = false;
+        boolean allneutrals = true;
         for (UnitInfo enemy : in.staticVariables.allenemies) {
+            if (enemy.getTeam() == in.staticVariables.opponent) {
+                allneutrals = false;
+            }
             Location enemyLoc = enemy.getLocation();
             boolean isWaterObs = in.helper.isObstructedWater(enemyLoc, in.staticVariables.myLocation);
-            if (in.staticVariables.allenemies.length > 8) {
-                if (isWaterObs) {
-                    int[] objective;
-                    if (enemy.getType() == UnitType.TOWER) {
-                        objective = in.objectives.createTowerObjective(enemyLoc);
-                    } else {
-                        objective = in.objectives.createWaterObjective(enemyLoc);
-                    }
-                    in.memoryManager.addObjective(UnitType.CATAPULT, objective);
+            if (isWaterObs) {
+                int[] objective;
+                if (enemy.getType() == UnitType.TOWER) {
+                    objective = in.objectives.createTowerObjective(enemyLoc);
+                } else {
+                    objective = in.objectives.createWaterObjective(enemyLoc);
                 }
+                in.memoryManager.addObjective(UnitType.CATAPULT, objective);
             }
             if (!in.unitController.isObstructed(enemyLoc, in.staticVariables.myLocation) && !isWaterObs) {
                 enemies = true;
@@ -83,9 +86,16 @@ public class Soldier {
 
         int bestIndex = -1;
 
-        for (int i = 8; i >= 0; i--) {
-            if (!in.unitController.canMove(in.staticVariables.dirs[i])) continue;
-            if (bestIndex < 0 || !microInfo[bestIndex].isBetter(microInfo[i])) bestIndex = i;
+        if (allneutrals) {
+            for (int i = 8; i >= 0; i--) {
+                if (!in.unitController.canMove(in.staticVariables.dirs[i])) continue;
+                if (bestIndex < 0 || !microInfo[bestIndex].isBetterNeutral(microInfo[i])) bestIndex = i;
+            }
+        } else {
+            for (int i = 8; i >= 0; i--) {
+                if (!in.unitController.canMove(in.staticVariables.dirs[i])) continue;
+                if (bestIndex < 0 || !microInfo[bestIndex].isBetter(microInfo[i])) bestIndex = i;
+            }
         }
 
         if (bestIndex != -1) {
@@ -110,6 +120,14 @@ public class Soldier {
         }
 
         void update(UnitInfo unit) {
+            int distance = unit.getLocation().distanceSquared(loc);
+            if (distance <= unit.getType().attackRangeSquared || (unit.getType() == UnitType.MAGE && distance < 14)) {
+                ++numEnemies;
+            }
+            if (distance < minDistToEnemy) minDistToEnemy = distance;
+        }
+
+        void updateArea() {
             if (!in.memoryManager.isLocationSafe(loc)) {
                 numEnemies += 100;
                 return;
@@ -118,11 +136,6 @@ public class Soldier {
                 numEnemies += 100;
                 return;
             }
-            int distance = unit.getLocation().distanceSquared(loc);
-            if (distance <= unit.getType().attackRangeSquared || (unit.getType() == UnitType.MAGE && distance < 14)) {
-                ++numEnemies;
-            }
-            if (distance < minDistToEnemy) minDistToEnemy = distance;
         }
 
         boolean canAttack() {
@@ -131,9 +144,24 @@ public class Soldier {
 
         boolean isBetter(MicroInfo m) {
             if (!in.memoryManager.isLocationSafe(m.loc)) return true;
-            if (2 * in.staticVariables.allyUnits.length < numEnemies) return false;
             if (canAttack()) {
                 if (!m.canAttack()) return true;
+                if (numEnemies < m.numEnemies) return true;
+                if (numEnemies > m.numEnemies) return false;
+                return minDistToEnemy >= m.minDistToEnemy;
+            }
+            if (m.canAttack()) return false;
+            return minDistToEnemy <= m.minDistToEnemy;
+        }
+
+        boolean isBetterNeutral(MicroInfo m) {
+            if (!in.memoryManager.isLocationSafe(m.loc)) return true;
+            if (in.staticVariables.allyUnits.length < 3 * numEnemies) return false;
+            if (!in.unitController.canAttack()) return minDistToEnemy >= m.minDistToEnemy;
+            if (canAttack()) {
+                if (!m.canAttack()) return true;
+                if (numEnemies < m.numEnemies) return true;
+                if (numEnemies > m.numEnemies) return false;
                 return minDistToEnemy >= m.minDistToEnemy;
             }
             if (m.canAttack()) return false;
